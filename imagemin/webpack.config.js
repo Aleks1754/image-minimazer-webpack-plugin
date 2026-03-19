acb9b5bde2d7273bd9ba89bce1c5aa4c08f8abcf
Oleksandr Kholod <oleksandr.kholod@uni-jena.de>
2026-03-19 12:24:31 +0100

    configuriert two learning Projects with ImageMinimizerWebpackPlugin(first with Imagemin, second with Sharp + SVGO)

---
+++ imagemin/webpack.config.js
@@ -0,0 +1,76 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import ImageMinimizerPlugin from "image-minimizer-webpack-plugin";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+
+export default (env)=> {
+    return {
+        mode: env.mode ?? "development",
+
+        entry: "./src/index.js",
+
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].min.js',
+            clean: true,
+        },
+
+        devtool: false,
+
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./src/index.html",
+                minify: false
+            }),
+        ],
+
+        module: {
+            rules: [
+
+                // CSS
+                {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader'],
+                },
+
+                // Images
+                {
+                    test: /\.(jpe?g|png|gif|svg)$/i,
+                    type: 'asset/resource',
+                    generator: {
+                        filename: 'images/[name].min.[ext]'
+                    },
+                }
+            ],
+        },
+
+        devServer: {
+            port: 3000,
+            open: true,
+        },
+        optimization: {
+            minimizer: [
+                "...",
+                new ImageMinimizerPlugin({
+                    minimizer: {
+                        implementation: ImageMinimizerPlugin.imageminMinify,
+                        options: {
+                            plugins: [
+                                ["gifsicle", { interlaced: true, colors: 64, optimizationLevel: 9 }],
+                                ["mozjpeg", { quality: 75 }],
+                                ["pngquant", { quality: [0.6, 0.8] }],
+                                ["svgo", { plugins: [{ name: "preset-default" }] }]
+                            ],
+                        },
+                    }
+                })
+            ],
+        },
+    }
+};

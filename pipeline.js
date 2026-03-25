d506b9291e65e3ebbede5870952b2bb0bb25b726
Oleksandr Kholod <oleksandr.kholod@uni-jena.de>
2026-03-25 11:49:07 +0100

    changed pipeline.js

--- pipeline.js
+++ pipeline.js
@@ -9,29 +9,59 @@ const rootDir = __dirname;
 
 const jobs = [
     {
-        name: 'imagemin',
+        name: 'ImageMinimizerWebpackPlugin(Imagemin)',
         cwd: path.join(rootDir, 'Webpack', 'imagemin'),
         command: 'npm',
         args: ['run', 'build'],
+        bundlePath: "./Webpack/imagemin/dist/",
+        bundleImagesPath: "./Webpack/imagemin/dist/images/",
     },
     {
-        name: "sharp",
+        name: "ImageMinimizerWebpackPlugin(Sharp + SVGO)",
         cwd: path.join(rootDir, 'Webpack', 'sharp'),
         command: 'npm',
         args: ['run', 'build'],
+        bundlePath: "./Webpack/sharp/dist/",
+        bundleImagesPath: "./Webpack/sharp/dist/images/",
     },
+    {
+        name: "vite-image-optimizer(Sharp + SVGO)",
+        cwd: path.join(rootDir, 'vite', 'vite-image-optimizer'),
+        command: 'npm',
+        args: ['run', 'build'],
+        bundlePath: "./vite/vite-image-optimizer/dist/",
+        bundleImagesPath: "./vite/vite-image-optimizer/dist/images/",
+    },
+    {
+        name: "vite-plugin-imagemin",
+        cwd: path.join(rootDir, 'vite', 'vite-plugin-imagemin'),
+        command: 'npm',
+        args: ['run', 'build'],
+        bundlePath: "./vite/vite-plugin-imagemin/dist/",
+        bundleImagesPath: "./vite/vite-plugin-imagemin/dist/images/",
+    },
+
 ];
 
-const folderPaths = {
-    original: "./Webpack/sharp/src/images/",
-    imageminBundle: "./Webpack/imagemin/dist/images/",
-    sharpBundle: "./Webpack/sharp/dist/images/",
-};
+function getFolderSize(dirPath) {
+    if (!fs.existsSync(dirPath)) return 0;
 
-// Run jobs synchronously
+    let totalSize = 0;
+    const files = fs.readdirSync(dirPath);
+
+    for (const file of files) {
+        const filePath = path.join(dirPath, file);
+        const stats = fs.statSync(filePath);
+
+        if (stats.isFile()) totalSize += stats.size;
+        else if (stats.isDirectory()) totalSize += getFolderSize(filePath);
+    }
+    return totalSize;
+}
+
 function main() {
-    const totalStart = Date.now();
 
+    const totalStart = Date.now();
     for (const job of jobs) {
         const start = Date.now();
 
@@ -56,46 +86,26 @@ function main() {
             return;
         }
 
-
-        const folderKey = job.name === "imagemin" ? "imageminBundle" : "sharpBundle";
         job.results = {
-            size: getFolderSize(folderPaths[folderKey]),
+            size: getFolderSize(job.bundlePath),
+            imagesSize: getFolderSize(job.bundleImagesPath),
             time: durationMs,
         };
     }
-
     const totalDuration = Date.now() - totalStart;
+
+    const originalImagesSize = getFolderSize("./Webpack/sharp/src/images/");
+
     console.log(`\n=== SUMMARY ===`);
     console.log(`Total time: ${(totalDuration / 1000).toFixed(2)}s`);
-    console.log(`Images directory size before optimisation: ${getFolderSize(folderPaths.original)} Bytes:`);
-    console.log(`≈ ${(getFolderSize(folderPaths.original) / 1024 / 1024).toFixed(2)} MB`);
+    console.log(`Original images size: ${originalImagesSize} Bytes ≈ ${(originalImagesSize / 1024 / 1024).toFixed(2)} MB`);
 
-    jobs.forEach((job, index) => {
-        console.log(`\nJob ${index + 1}: ${job.name}`);
+    jobs.forEach((job) => {
+        console.log(`\n=== ${job.name} ===`);
         console.log(`Time: ${(job.results.time / 1000).toFixed(2)}s`);
-        console.log(`Optimised images size: ${job.results.size} Bytes`);
-        console.log(`≈ ${(job.results.size / 1024 / 1024).toFixed(2)} MB`);
+        console.log(`Bundle size: ${job.results.size} Bytes ≈ ${(job.results.size / 1024 / 1024).toFixed(2)} MB`);
+        console.log(`Optimised images size: ${job.results.imagesSize} Bytes ≈ ${(job.results.imagesSize / 1024 / 1024).toFixed(2)} MB`);
     });
-}
-
-
-function getFolderSize(dirPath) {
-    if (!fs.existsSync(dirPath)) return 0;
-
-    let totalSize = 0;
-    const files = fs.readdirSync(dirPath);
-
-    for (const file of files) {
-        const filePath = path.join(dirPath, file);
-        const stats = fs.statSync(filePath);
-
-        if (stats.isFile()) {
-            totalSize += stats.size;
-        } else if (stats.isDirectory()) {
-            totalSize += getFolderSize(filePath);
-        }
-    }
-    return totalSize;
 }
 
 main();
